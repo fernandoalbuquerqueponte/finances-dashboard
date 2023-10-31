0020028aad0b3e461d57da59af2da12e5e78b877
Fernando Neto <fernandonetodeveloper@gmail.com>
2023-10-31 16:57:00 -0300

    💄 feat: configuração do tema e themeprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter } from "react-router-dom"
 import AppRoutes from "./routes/routes"
 
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/theme/default"
+
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
---
+++ src/styles/theme/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+   COLORS: {
+      white: '#FFF',
+      blue: '#2563EB',
+      dark_blue: '#1F2937',
+      green: '#16A34A',
+      dark_green: '#066045',
+      red: '#DC2626',
+      dark_red: '#991B1B',
+      gray_200: '#c4c4c4',
+      gray_400: '#262626',
+      gray_900: '#121214',
+   },
+   FONT_SIZE: {
+      xs: 16,
+      sm: 25,
+      md: 30,
+      lg: 40,
+      xl: 50,
+   }
+}
