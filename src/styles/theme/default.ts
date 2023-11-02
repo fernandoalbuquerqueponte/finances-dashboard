516318ed65bbc816a8682d92f9e211a518d055a2
Fernando Neto <fernandonetodeveloper@gmail.com>
2023-11-02 15:24:40 -0300

    💄feat: mudança no tema da aplicação

--- src/styles/theme/default.ts
+++ src/styles/theme/default.ts
@@ -1,21 +1,14 @@
 export const defaultTheme = {
    COLORS: {
-      white: '#FFF',
-      blue: '#2563EB',
-      dark_blue: '#1F2937',
-      green: '#16A34A',
-      dark_green: '#066045',
-      red: '#DC2626',
-      dark_red: '#991B1B',
-      gray_200: '#c4c4c4',
-      gray_400: '#262626',
-      gray_900: '#121214',
-   },
-   FONT_SIZE: {
-      xs: 16,
-      sm: 25,
-      md: 30,
-      lg: 40,
-      xl: 50,
+      white: '#fafafa',
+      neutral300: '#d4d4d4',
+      neutral900: '#171717',
+      gray900: '#121214',
+      red900: '#7f1d1d',
+      green600: '#16a34a',
+      green900: '#14532d',
+      blue900: '#1e3a8a',
+      violet900: '#4c1d95',
+
    }
 }
