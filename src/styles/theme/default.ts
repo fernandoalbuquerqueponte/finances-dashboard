db671891453cd3f38e8b400f4b3c231a5da3e607
Fernando Neto <fernandonetodeveloper@gmail.com>
2023-11-03 21:00:28 -0300

    💄feat: novas cores no tema

--- src/styles/theme/default.ts
+++ src/styles/theme/default.ts
@@ -3,12 +3,13 @@ export const defaultTheme = {
       white: '#fafafa',
       neutral300: '#d4d4d4',
       neutral900: '#171717',
+      gray700: '#7C7C8',
       gray900: '#121214',
+      red700: '#DC2626',
       red900: '#7f1d1d',
       green600: '#16a34a',
       green900: '#14532d',
       blue900: '#1e3a8a',
       violet900: '#4c1d95',
-
    }
 }
