81990453ffb79bbe5f645487da4434b6c7a45924
Fernando Neto <fernandonetodeveloper@gmail.com>
2023-11-15 16:03:39 -0300

    ✨feat: controle de rotas públicas e privadas

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,12 +2,19 @@ import { Route, Routes } from "react-router-dom";
 
 import { SignIn } from "../pages/SignIn";
 import { SignUp } from "../pages/SignUp";
+import { Dashboard } from "../pages/Dashboard";
+import { Private } from "./Private";
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<SignIn />} />
             <Route path="/signUp" element={<SignUp />} />
+            <Route path="/dashboard" element={
+                <Private>
+                    <Dashboard />
+                </Private>
+            } />
         </Routes>
     )
 }
