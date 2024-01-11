c6669a9ca408dbf326bd2c7f30066aaf6c0e64ce
Fernando Neto <fernandonetodeveloper@gmail.com>
2024-01-11 14:54:27 -0300

    💄feat: estilização da tela new

--- src/pages/New/New.styled.ts
+++ src/pages/New/New.styled.ts
@@ -1,1 +1,42 @@
 import styled from "styled-components";
+
+export const NewContainer = styled.div`
+   >form {
+      display: flex;
+      flex-direction: column;
+      margin: 40px 20px;
+      max-width:  700px;
+
+      >label{
+         color: ${({ theme }) => theme.COLORS.white};
+         font-size: 1rem;
+         padding-left: 0.6rem;
+         margin-bottom: 5px;
+      }
+
+      >textarea {
+         max-width: 700px;
+         background-color: #1e1e1e;
+         color: #fafafa;
+         font-size: 16px;
+         padding: 10px;
+         height: 150px;
+         border-radius: 7px;
+         border: none;
+      }
+   }
+`
+
+export const RadioInputContainer = styled.div`
+   display: flex;
+   color: #fafafa;
+
+   >input[type="radio"]{
+      width: 16px;
+      cursor: pointer;
+   }
+
+   >span {
+      margin: 0 15px;
+   }
+`
--- src/pages/New/index.tsx
+++ src/pages/New/index.tsx
@@ -1,10 +1,75 @@
+import React, { useState, useContext } from 'react';
+import { AuthContext } from '../../contexts/auth';
+
+import { db } from '../../services/firebaseConfig';
+import { addDoc, collection, doc, updateDoc } from 'firebase/firestore';
+
 import { Header } from '../../components/Header'
+import { Title } from '../../components/Title'
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
 import * as S from './New.styled'
 
 export const New = () => {
+   const { user } = useContext(AuthContext)
+
+   const [type, setType] = useState("entrada")
+   const [description, setDescription] = useState("")
+   const [value, setValue] = useState("")
+
+   const handleChangeType = (e: any) => setType(e.target.value)
+
+   const handleSubmit = (e: any) => {
+      e.preventDefault()
+   }
+
    return (
-      <>
+      <S.NewContainer>
          <Header />
-      </>
+         <Title title='Adicionar' />
+
+         <form onSubmit={handleSubmit}>
+            <label>Tipo de finança:</label>
+            <S.RadioInputContainer>
+               <input
+                  type="radio"
+                  value="entrada"
+                  onChange={handleChangeType}
+                  checked={type === "entrada"}
+               />
+               <span>Entrada</span>
+
+               <input
+                  type="radio"
+                  value="saída"
+                  onChange={handleChangeType}
+                  checked={type === "saída"}
+               />
+               <span>Saída</span>
+            </S.RadioInputContainer>
+            <Input
+               placeholder='Ex: $4000,00'
+               value={value}
+               onChange={(e) => setValue(e.target.value)}
+               type='number'
+               label='Valor'
+            />
+            <label>Descrição</label>
+            <textarea
+               placeholder='Descreva sua descrição(opcional)'
+               value={description}
+               onChange={(e) => setDescription(e.target.value)}
+            />
+            <br />
+            <Button
+               name='Salvar'
+               color='success'
+               type='submit'
+               width={90}
+               height={50}
+            />
+         </form>
+      </S.NewContainer>
    )
 }
