a206cdf366aa042b904e740ce446759ce1263a75
Fernando Neto <fernandonetodeveloper@gmail.com>
2024-01-12 08:42:35 -0300

    🔧chore: configuração do react-toastfy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter } from "react-router-dom"
 import AppRoutes from "./routes/routes"
 
+import { ToastContainer } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css"
+
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/theme/default"
 import { GlobalStyle } from "./styles/global"
@@ -11,6 +14,7 @@ function App() {
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
         <AuthProvider>
+          <ToastContainer autoClose={3000} />
           <AppRoutes />
         </AuthProvider>
       </BrowserRouter>
