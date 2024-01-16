6fe8733c043458178e05df3c1b423e275fd44d9a
Fernando Neto <fernandonetodeveloper@gmail.com>
2024-01-15 22:20:24 -0300

    💄feat: estilização do componente TransactionCard

---
+++ src/components/TransactionCard/TransactionCard.styled.ts
@@ -0,0 +1,58 @@
+import styled, { css } from "styled-components";
+import { ArrowCircleUp, ArrowCircleDown } from "@phosphor-icons/react";
+
+import { TransactionCardProps } from './index';
+
+export const TransactionCard = styled.div<TransactionCardProps>`
+   display: flex;
+   width: 80%;
+   margin: 10px auto;
+   flex-direction: column;
+
+   >div {
+      display: flex;
+      align-items: center;
+      padding: 0 15px;
+      max-width: 100%;
+      height: 60px;
+      margin: 15px 0;
+      border-radius: 15px;
+      cursor: pointer;
+      background-color: ${({ theme }) => theme.COLORS.neutral900};
+
+      >div {
+         display: flex;
+         width: 100%;
+         justify-content: space-between;
+         padding: 0 16px;
+         color: #c4c4c4;
+      }
+
+   }
+
+   >p {
+      color: ${({ theme }) => theme.COLORS.neutral300};
+   }
+`
+
+export const Entrace = styled.span`
+   color: ${({ theme }) => theme.COLORS.green600};
+   font-weight: 600;
+`;
+
+export const OutFlow = styled.span`
+   color: ${({ theme }) => theme.COLORS.red700};
+   font-weight: 600;
+`;
+
+export const CashOutFlowIcon = styled(ArrowCircleDown).attrs({
+   size: 30,
+})`
+color: ${({ theme }) => theme.COLORS.red700};
+`
+
+export const CashEntraceIcon = styled(ArrowCircleUp).attrs({
+   size: 30,
+})`
+color: ${({ theme }) => theme.COLORS.green600};
+`;
---
+++ src/components/TransactionCard/index.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import * as S from "./TransactionCard.styled"
+
+export interface TransactionCardProps {
+   financeName?: string
+   type?: 'entrada' | 'saída'
+   date?: string
+   value?: string
+}
+
+export const TransactionCard: React.FC<TransactionCardProps> = ({ date , financeName, type = "entrada", value }) => {
+   return (
+      <S.TransactionCard>
+         {date && <p>{date}</p>}
+         <div>
+            {type === 'entrada' ? <S.CashEntraceIcon /> : <S.CashOutFlowIcon />}
+            <div>
+               <span>{financeName}</span>
+               {type === 'entrada' && <S.Entrace>{value}</S.Entrace>}
+               {type === 'saída' && <S.OutFlow>{value}</S.OutFlow>}
+            </div>
+         </div>
+      </S.TransactionCard>
+   )
+}
