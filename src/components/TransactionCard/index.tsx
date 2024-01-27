e371d6640fcf669ca138da44cb2c94ed86dc316b
Fernando Neto <fernandonetodeveloper@gmail.com>
2024-01-27 00:59:51 -0300

    🏷️feat: mudando tipagem e formatando valor para BRL

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import * as S from "./TransactionCard.styled"
 
+import { currencyFormatter } from "../../utils/currencyFormatter"
+
 export interface TransactionCardProps {
    financeName?: string
    type?: 'entrada' | 'saída'
@@ -8,7 +10,7 @@ export interface TransactionCardProps {
    value?: string
 }
 
-export const TransactionCard: React.FC<TransactionCardProps> = ({ date, financeName, type, value }) => {
+export const TransactionCard: React.FC<TransactionCardProps> = ({ date, financeName, type, value = "" }) => {
    return (
       <S.TransactionCard>
          {date && <p>{date}</p>}
@@ -16,8 +18,8 @@ export const TransactionCard: React.FC<TransactionCardProps> = ({ date, financeN
             {type === 'entrada' ? <S.CashEntraceIcon /> : <S.CashOutFlowIcon />}
             <div>
                <span>{financeName}</span>
-               {type === 'entrada' && <S.Entrace>{value}</S.Entrace>}
-               {type === 'saída' && <S.OutFlow>{value}</S.OutFlow>}
+               {type === 'entrada' && <S.Entrace>{currencyFormatter(value)}</S.Entrace>}
+               {type === 'saída' && <S.OutFlow>{currencyFormatter(value)}</S.OutFlow>}
             </div>
          </div>
       </S.TransactionCard>
