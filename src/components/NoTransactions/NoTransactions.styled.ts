2336529bad8541c8bf48012fba3ecc7cfb111b94
Fernando Neto <fernandonetodeveloper@gmail.com>
2024-03-27 16:28:04 -0300

    fix: ensure NoTransactionContainer does not exceed its limit

--- src/components/NoTransactions/NoTransactions.styled.ts
+++ src/components/NoTransactions/NoTransactions.styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const NoTransactionsContainer = styled.div`
   display: flex;
   flex-direction: column;
-  max-width: 100%;
+  max-width: 80vw;
   justify-content: center;
   padding: 30px;
   border: 1px solid #c4c4c4;
